22de03a18087e996afc2d187298334639242519c
LeoCraftTV <lyoniya11@mail.ru>
2022-06-02 17:22:59 +0300

    Release

--- newdefs.py
+++ newdefs.py
@@ -22,38 +22,72 @@ def Nowdate():
 	return(date_time)	
 
 def Preresult(TON, TOB):
-	global date
-	wbSearch = Workbook()
-	wbSearch = load_workbook(pa.Paths.FilePath)
-	wsSearch = wbSearch.active
-	for i in range(1,10):
-		for i1 in range(1,15):
-			value=wsSearch.cell(row=i, column=i1).value
-			if value == TOB:
-				BalanceColumn = i1
-				BalanceRaw = i
-	for i in range(1,10):
-		for i1 in range(1,15):
-			value=wsSearch.cell(row=i, column=i1).value
-			if value == TON:
-				NameColumn = i1
-				NameRaw = i
-	wbResult = Workbook()
-	wsResult = wbResult.active
-	i1=NameRaw-1
-	i2=1
-	value1 = wsSearch.cell(row= NameRaw, column = NameColumn).value 
-	value2 = wsSearch.cell(row= BalanceRaw, column = BalanceColumn).value
-	for i in range(BalanceRaw, wsSearch.max_row):
-		value1 = wsSearch.cell(row=i, column=NameColumn).value 
-		value2 = wsSearch.cell(row=i, column=BalanceColumn).value
-		if wsSearch.cell(row=i, column=NameColumn).value is not None:
-			wsResult.cell(row=i2, column=1).value=value1
-			wsResult.cell(row=i2, column=2).value=value2
-			i2+=1
-	wbEnd = Workbook()
-	wsEnd = wbEnd.active
-	wbResult.save(str(pa.Paths.PreresultFilePath))
+	try:
+		global date
+		try:
+			wbSearch = Workbook()
+			wbSearch = load_workbook(pa.Paths.FilePath)
+			wsSearch = wbSearch.active
+		except:
+			print("Пизда 1")
+		try:
+			print(TOB)
+			print()
+			for i in range(1,10):
+				for i1 in range(1,30):
+					value=wsSearch.cell(row=i, column=i1).value
+					print(value)
+					if value == TOB:
+						pa.ColumnsIDs.BalanceColumn = i1
+						pa.ColumnsIDs.BalanceRaw = i
+						break
+		except: 
+			print("Пизда 2")
+		try:
+			print(TON)
+			print()
+			for i in range(1,10):
+				for i1 in range(1,30):
+					value=wsSearch.cell(row=i, column=i1).value
+					print(value)
+					if value == TON:
+						pa.ColumnsIDs.NameColumn = i1
+						pa.ColumnsIDs.NameRaw = i
+						break
+		except:
+			print("Пизда 3")
+		try:
+			print(pa.ColumnsIDs.BalanceColumn, pa.ColumnsIDs.BalanceRaw)
+			print(pa.ColumnsIDs.NameColumn, pa.ColumnsIDs.NameRaw)
+			wbResult = Workbook()
+			wsResult = wbResult.active
+			i1=pa.ColumnsIDs.NameRaw-1
+			i2=1
+			value1 = wsSearch.cell(row= pa.ColumnsIDs.NameRaw, column = pa.ColumnsIDs.NameColumn).value
+			print(wsSearch.cell(row= pa.ColumnsIDs.NameRaw, column = pa.ColumnsIDs.NameColumn))
+			print(pa.ColumnsIDs.NameRaw, pa.ColumnsIDs.NameColumn)
+			value2 = wsSearch.cell(row= pa.ColumnsIDs.BalanceRaw, column = pa.ColumnsIDs.BalanceColumn).value
+			print(wsSearch.cell(row= pa.ColumnsIDs.BalanceRaw, column = pa.ColumnsIDs.BalanceColumn))
+			print(pa.ColumnsIDs.BalanceRaw, pa.ColumnsIDs.BalanceColumn)
+		except:
+			print("Пизда 4")
+		try:
+			for i in range(pa.ColumnsIDs.BalanceRaw, wsSearch.max_row):
+				value1 = wsSearch.cell(row=i, column=pa.ColumnsIDs.NameColumn).value 
+				value2 = wsSearch.cell(row=i, column=pa.ColumnsIDs.BalanceColumn).value
+				if wsSearch.cell(row=i, column=pa.ColumnsIDs.NameColumn).value is not None:
+					wsResult.cell(row=i2, column=1).value=value1
+					wsResult.cell(row=i2, column=2).value=value2
+					i2+=1
+		except:
+			print("Пизда 5")
+		try:
+			wbEnd = Workbook()
+			wsEnd = wbEnd.active
+			wbResult.save(str(pa.Paths.PreresultFilePath))
+		except:
+			print("Пизда 6")
+	except: print("Пизда")
 
 def DataProcessing(Name):
 	global TableI
